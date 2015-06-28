9b6bd73de643b15e15ba51a80e8efeeeda254a0c
David Neubauer <davidneub@gmail.com>
2015-06-28 13:43:35 +0200

    introduced getScore and incrementScore

--- tennis.js
+++ tennis.js
@@ -2,13 +2,21 @@ var Player = function() {
     this.score = 0;
 };
 
+Player.prototype.getScore = function() {
+    return this.score;
+};
+
+Player.prototype.incrementScore = function() {
+    this.score++;
+};
+
 var TennisGame = function() {
     this.player1 = new Player();
     this.player2 = new Player();
 };
 
 TennisGame.prototype.wonPoint = function(playerName) {
-    playerName === "player1" ? this.player1.score += 1 : this.player2.score += 1;
+    playerName === "player1" ? this.player1.incrementScore() : this.player2.incrementScore();
 };
 
 TennisGame.prototype.getScoreName = function(scorePlayer) {
@@ -16,7 +24,7 @@ TennisGame.prototype.getScoreName = function(scorePlayer) {
 };
 
 TennisGame.prototype.getEqualScoreName = function() {
-    return ['Love-All', 'Fifteen-All', 'Thirty-All'][this.player1.score] || 'Deuce';
+    return ['Love-All', 'Fifteen-All', 'Thirty-All'][this.player1.getScore()] || 'Deuce';
 };
 
 TennisGame.prototype.getAdvantageName = function(minusResult) {
@@ -28,7 +36,7 @@ TennisGame.prototype.getWinnerName = function(minusResult) {
 };
 
 TennisGame.prototype.getOverFourName = function() {
-    var minusResult = this.player1.score - this.player2.score;
+    var minusResult = this.player1.getScore() - this.player2.getScore();
     return this.isAdvantage(minusResult) ? this.getAdvantageName(minusResult) : this.getWinnerName(minusResult);
 };
 
@@ -37,11 +45,11 @@ TennisGame.prototype.isAdvantage = function(minusResult) {
 };
 
 TennisGame.prototype.isOverFour = function() {
-    return this.player1.score >= 4 || this.player2.score >= 4;
+    return this.player1.getScore() >= 4 || this.player2.getScore() >= 4;
 };
 
 TennisGame.prototype.isEqualScore = function() {
-    return this.player1.score === this.player2.score;
+    return this.player1.getScore() === this.player2.getScore();
 };
 
 TennisGame.prototype.getScore = function() {
@@ -50,7 +58,7 @@ TennisGame.prototype.getScore = function() {
     } else if (this.isOverFour()) {
         return this.getOverFourName();
     }
-    return this.getScoreName(this.player1.score) + "-" + this.getScoreName(this.player2.score);
+    return this.getScoreName(this.player1.getScore()) + "-" + this.getScoreName(this.player2.getScore());
 };
 
 export default TennisGame;
